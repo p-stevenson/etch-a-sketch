5bbe400ace06b6c07c605c68fb07c5693846ccf5
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-04-23 09:41:09 +0100

    Major rewrite
    Rewrite draw functions it makes more sense in this case to apply them to the parent.
    Rewrite clear function to simply reset colour instead of redrawing grid.
    Add removeEventListener where appropriate to improve performance and to allow click
    then draw functionality.
    Add setRGB function to avoid repeated code when setting rgb values.
    Add listen function to listen for various events like button activations, instead
    of calling a list of functions on page load.
    Add random number function for random color generation.
    Remove triggerMouse function in favour invoking click event listeners button press.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,121 +1,131 @@
-const slider = document.querySelector('#gridSlider');
 const sliderValueText = document.querySelector('#gridSliderText');
+const gridContainer = document.querySelector('#gridContainer');
+const colorSwatch = document.querySelector('.colorSwatch');
+const slider = document.querySelector('#gridSlider');
+const redValue = document.querySelector('#redSlider');
+const greenValue = document.querySelector('#greenSlider');
+const blueValue = document.querySelector('#blueSlider');
 
-slider.oninput = function() {
-    const gridContainer = document.querySelector('.gridContainer');
-    const gridDivisions =  slider.value ** 2; 
+function listen() {
+    const clearButton = document.querySelector('#clearButton');
+    const blackButton = document.querySelector('#blackButton');
+    const multiButton = document.querySelector('#multiButton');;
+
+    clearButton.addEventListener('click', event => {
+        gridContainer.removeEventListener('mouseover', draw);
+        gridContainer.removeEventListener('mouseover', drawMulti);
+        gridContainer.removeEventListener('mouseover', drawBlack);
+        clear();
+    });
+    blackButton.addEventListener('click', event => {
+        gridContainer.addEventListener('click', e => {
+            gridContainer.removeEventListener('mouseover', draw);
+            gridContainer.removeEventListener('mouseover', drawMulti);
+            gridContainer.addEventListener('mouseover', drawBlack);
+        });
+    });
+    multiButton.addEventListener('click', event => {
+        gridContainer.addEventListener('click', e => {
+            gridContainer.removeEventListener('mouseover', draw);
+            gridContainer.removeEventListener('mouseover', drawBlack);
+            gridContainer.addEventListener('mouseover', drawMulti);
+        });
+    });
+    gridContainer.addEventListener('click', e => {
+        gridContainer.removeEventListener('mouseover', drawBlack);
+        gridContainer.removeEventListener('mouseover', drawMulti);
+        gridContainer.addEventListener('mouseover', draw);
+    });
+}
+
+slider.oninput = function () {
+    clearGridContainer();
+    const gridDivisions = slider.value ** 2;
     sliderValueText.textContent = `GRID SIZE: ${slider.value}`;
     gridContainer.style.setProperty('grid-template-columns', `repeat(
             ${slider.value}, minmax(auto, auto))`);
-    
-    clearGridContainer();
-
     for (let i = 0; i < gridDivisions; i++) {
         const gridDiv = document.createElement('div');
         gridDiv.classList.add('gridDiv');
         gridContainer.appendChild(gridDiv);
     }
-    triggerMouse();
-}
-
-function clear() {
-    const clearButton = document.querySelector('#clearButton');
-    clearButton.addEventListener('click', e => {
-        slider.oninput();
-    });
-}
-
-function setBlack() {
-    const colorSwatch = document.querySelector('.colorSwatch');
-    const redValue = document.querySelector('#redSlider');
-    const greenValue = document.querySelector('#greenSlider');
-    const blueValue = document.querySelector('#blueSlider');
-
-    const blackButton = document.querySelector('#blackButton');
-    blackButton.addEventListener('click', e => {
-        redValue.value = 0;
-        greenValue.value = 0;
-        blueValue.value = 0;
-        colorSwatch.style.setProperty('background-color', `rgb(${redValue.value}, 
-            ${greenValue.value}, ${blueValue.value})`);
-    })
-}
-
-function setRandom() {
-    const colorSwatch = document.querySelector('.colorSwatch')
-    const redValue = document.querySelector('#redSlider');
-    const greenValue = document.querySelector('#greenSlider');
-    const blueValue = document.querySelector('#blueSlider');
-
-    const randomButton = document.querySelector('#randomButton');
-    randomButton.addEventListener('click', e => {
-        redValue.value = Math.floor(Math.random() * 255);
-        greenValue.value = Math.floor(Math.random() * 255);
-        blueValue.value = Math.floor(Math.random() * 255);
-        colorSwatch.style.setProperty('background-color', `rgb(${redValue.value}, 
-            ${greenValue.value}, ${blueValue.value})`);
-        console.log('test');
-    })
+    listen();
 }
 
 function clearGridContainer() {
     const gridContainer = document.querySelector('.gridContainer');
     const allGridDivs = document.querySelectorAll('.gridDiv');
-
     allGridDivs.forEach((div) => {
         gridContainer.removeChild(div);
     });
-
 }
 
-function draw() {
-    const mouseTarget = document.querySelectorAll('.gridDiv');
-    mouseTarget.forEach((div) => {
-        div.addEventListener('mouseenter', e => {
-            div.style.backgroundColor = colorSwatch.style.getPropertyValue('background-color');
-        });
+function clear() {
+    const allGridDivs = document.querySelectorAll('.gridDiv');
+    allGridDivs.forEach((div) => {
+        div.style.backgroundColor = 'rgb(206, 206, 206)';
     });
+    colorSwatch.style.setProperty('background-color', 'rgb(128,128,128');
+    resetRGB();
 }
 
-function triggerMouse() {
-    const mouseTarget = document.querySelectorAll('.gridDiv');
-    mouseTarget.forEach((div) => {
-        div.addEventListener('mousedown', e => {
-            draw();
-        })
-    })
+function random(number) {
+    return Math.floor(Math.random()* number);
 }
 
-triggerMouse();
+function draw(event) {
+    event.target.style.backgroundColor = colorSwatch.style.backgroundColor;
+    gridContainer.addEventListener('click', e => {
+        gridContainer.removeEventListener('mouseover', draw);
+    }, { once: true });
+}
 
-/* function colorPicker(); { */
-    const colorSwatch = document.querySelector('.colorSwatch');
-    const redValue = document.querySelector('#redSlider');
-    const greenValue = document.querySelector('#greenSlider');
-    const blueValue = document.querySelector('#blueSlider');
-/* } */
 
-redValue.oninput = function() {
+function drawMulti(event) {
+    const rndColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    event.target.style.backgroundColor = rndColor;
+    colorSwatch.style.backgroundColor = rndColor;
+    gridContainer.addEventListener('click', e => {
+        gridContainer.removeEventListener('mouseover', drawMulti);
+    }, { once: true });
+}
+
+function drawBlack(event) {
+    colorSwatch.style.setProperty('background-color', 'rgb(0, 0, 0');
+    redValue.value = 0;
+    greenValue.value = 0;
+    blueValue.value = 0;
+    event.target.style.backgroundColor = 'rgb(0,0,0)';
+    gridContainer.addEventListener('click', e => {
+        gridContainer.removeEventListener('mouseover', drawBlack);
+    }, { once: true });
+}
+
+function setRGB() {
+    gridContainer.removeEventListener('mouseover', drawBlack);
+    gridContainer.removeEventListener('mouseover', drawMulti);
+    gridContainer.removeEventListener('mouseover', draw);
     colorSwatch.style.setProperty('background-color', `rgb(${redValue.value}, 
-            ${greenValue.value}, ${blueValue.value})`);
+        ${greenValue.value}, ${blueValue.value})`);
+    listen();
 }
 
-greenValue.oninput = function() {
-    colorSwatch.style.setProperty('background-color', `rgb(${redValue.value}, 
-            ${greenValue.value}, ${blueValue.value})`);
+redValue.oninput = function () {
+    setRGB();
+}
+greenValue.oninput = function () {
+   setRGB();
+}
+blueValue.oninput = function () {
+    setRGB();
 }
 
-blueValue.oninput = function() {
-    colorSwatch.style.setProperty('background-color', `rgb(${redValue.value}, 
-            ${greenValue.value}, ${blueValue.value})`);
+function resetRGB() {
+    redValue.value = 128;
+    greenValue.value = 128;
+    blueValue.value = 128;
+    colorSwatch.style.setProperty('background-color', 'rgb(128,128,128');
 }
-clear();
-setBlack();
-setRandom();
 
-// general tidy up
-// move color picker out of global scope
-// implement method for random color on mouse enter
-
-
-
+slider.oninput();
+listen();
